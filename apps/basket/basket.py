f3e5c05d32266d3cf0e4f5e5f510c7bfbceb67a1
Bhuwansingh248 <Bhuwansingh248@gmail.com>
2021-08-25 01:14:43 +0530

    modified session data store quantity with session

--- apps/basket/basket.py
+++ apps/basket/basket.py
@@ -9,7 +9,25 @@ class Basket():
         self.session = request.session
         basket = self.session.get('session_key')  # creating cookie with session key
         if 'session_key' not in request.session:
-            basket = self.session['session_key'] = {'number':120913137912}
+            basket = self.session['session_key'] = {}
         self.basket = basket
 
         
+    def add(self, product, qty):
+        product_id = product.id
+        if product_id not in self.basket:
+            self.basket[product_id] = {
+                'price': float(product.price),
+                'qty' : int(qty)
+            }
+        self.session.modified = True
+        
+    def __len__(self):
+        '''
+            get the basket data and item qty
+            @ return 
+            all item 
+        '''
+        return sum(item['qty'] for item in self.basket.values())
+
+    
--- apps/basket/urls.py
+++ apps/basket/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from . import views
+from apps import basket
 
 app_name = 'basket'
 
 urlpatterns = [
-    path('' , views.basket_summary, name='basket_summary')
+    path('' , views.basket_summary, name='basket_summary'),
+    path('add/', views.basket_add, name = 'basket_add'),
 ]
 
